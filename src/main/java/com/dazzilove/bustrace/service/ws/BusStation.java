c1f5209dca38e2ccd6e05551b2c7b35a0081cce4
10028467 <dazzilove@sk.com>
2019-12-17 16:57:59 +0900

    star 아이콘 기능 개발 중

--- src/main/java/com/dazzilove/bustrace/service/ws/BusStation.java
+++ src/main/java/com/dazzilove/bustrace/service/ws/BusStation.java
@@ -3,6 +3,9 @@ package com.dazzilove.bustrace.service.ws;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 @ToString
 public class BusStation {
@@ -15,5 +18,20 @@ public class BusStation {
 	private String stationName;
 	private double x;
 	private double y;
+
+	private Map<String, String> startStationIdMap;
+
+	public BusStation() {
+		startStationIdMap = new HashMap<>();
+		startStationIdMap.put("224000142", "224000142");
+		startStationIdMap.put("224000141", "224000141");
+		startStationIdMap.put("224000137", "224000137");
+		startStationIdMap.put("224000059", "224000059");
+		startStationIdMap.put("224000703", "224000703");
+		startStationIdMap.put("224000050", "224000050");
+		startStationIdMap.put("224000171", "224000171");
+		startStationIdMap.put("224000783", "224000783");
+		startStationIdMap.put("277103206", "277103206");
+	}
 
 }

d786598f3720cd2c9ddb3629199c391cc0e0d66b
dazzilove <dazzilove@gmail.com>
2019-12-26 03:30:08 +0900

    - 차량타입 정보 전달 방식 변경 - 차량타입명 리턴 기능 추가

--- src/main/java/com/dazzilove/bustrace/utils/CodeUtil.java
+++ src/main/java/com/dazzilove/bustrace/utils/CodeUtil.java
@@ -2,56 +2,62 @@ package com.dazzilove.bustrace.utils;
 
 import com.dazzilove.bustrace.domain.PlateType;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CodeUtil {
 
-	static List<PlateType> plateTypes = new ArrayList<>();
+	static Map<String, PlateType> plateTypes = new HashMap<>();
 
-	public static List<PlateType> getPlateTypes() {
+	public static String getPlateTypeName(String code) {
+		return plateTypes.get(code).getName();
+	}
+
+	public static Map<String, PlateType> getPlateTypes() {
+		Map<String, PlateType> newPlateTypes = new HashMap<>();
+		PlateType plateType;
+
+		plateType = new PlateType();
+		plateType.setCode("0");
+		plateType.setName("정보없음");
+		plateType.setImageSrc("/img/bus1f.png");
+		newPlateTypes.put(plateType.getCode(), plateType);
+
+		plateType = new PlateType();
+		plateType.setCode("1");
+		plateType.setName("소형승합차");
+		plateType.setImageSrc("/img/bus1f_0.png");
+		newPlateTypes.put(plateType.getCode(), plateType);
+
+		plateType = new PlateType();
+		plateType.setCode("2");
+		plateType.setName("중형승합차");
+		plateType.setImageSrc("/img/bus1f_1.png");
+		newPlateTypes.put(plateType.getCode(), plateType);
+
+		plateType = new PlateType();
+		plateType.setCode("3");
+		plateType.setName("대형승합차");
+		plateType.setImageSrc("/img/bus1f_2.png");
+		newPlateTypes.put(plateType.getCode(), plateType);
+
+		plateType = new PlateType();
+		plateType.setCode("4");
+		plateType.setName("2층버스");
+		plateType.setImageSrc("/img/bus2f_2.png");
+		newPlateTypes.put(plateType.getCode(), plateType);
+
+		plateType = new PlateType();
+		plateType.setCode("99");
+		plateType.setName("전체");
+		plateType.setImageSrc("/img/bus.png");
+		newPlateTypes.put(plateType.getCode(), plateType);
+
 		if (plateTypes.isEmpty()) {
-			plateTypes = new ArrayList<>();
-			PlateType plateType;
-
-			plateType = new PlateType();
-			plateType.setCode("0");
-			plateType.setName("정보없음");
-			plateType.setImageSrc("/img/bus1f.png");
-			plateTypes.add(plateType);
-
-			plateType = new PlateType();
-			plateType.setCode("1");
-			plateType.setName("소형승합차");
-			plateType.setImageSrc("/img/bus1f_0.png");
-			plateTypes.add(plateType);
-
-			plateType = new PlateType();
-			plateType.setCode("2");
-			plateType.setName("중형승합차");
-			plateType.setImageSrc("/img/bus1f_1.png");
-			plateTypes.add(plateType);
-
-			plateType = new PlateType();
-			plateType.setCode("3");
-			plateType.setName("대형승합차");
-			plateType.setImageSrc("/img/bus1f_2.png");
-			plateTypes.add(plateType);
-
-			plateType = new PlateType();
-			plateType.setCode("4");
-			plateType.setName("2층버스");
-			plateType.setImageSrc("/img/bus2f_2.png");
-			plateTypes.add(plateType);
-
-			plateType = new PlateType();
-			plateType.setCode("99");
-			plateType.setName("전체");
-			plateType.setImageSrc("/img/bus.png");
-			plateTypes.add(plateType);
+			plateTypes = newPlateTypes;
 		}
 
-		return plateTypes;
+		return newPlateTypes;
 	}
 
 }

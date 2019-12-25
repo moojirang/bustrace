a715060f0c148d642e36f7451350d3b1d7a42bd5
dazzilove <dazzilove@gmail.com>
2019-12-26 02:04:59 +0900

    최초 커밋

---
+++ src/main/java/com/dazzilove/bustrace/domain/PlateType.java
@@ -0,0 +1,14 @@
+package com.dazzilove.bustrace.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class PlateType {
+	private String code;
+	private String name;
+	private String imageSrc;
+}
+
+

40bdd5678084b6bd0612e80fb116a425e65301c4
dazzilove <dazzilove@gmail.com>
2019-12-29 22:51:51 +0900

    임시로 사용하는 버스 리스트를 컨트롤러에서 정의하던것을 Bus 객체에서 정의하도록 수정 함

--- src/main/java/com/dazzilove/bustrace/domain/Bus.java
+++ src/main/java/com/dazzilove/bustrace/domain/Bus.java
@@ -4,6 +4,9 @@ import com.dazzilove.bustrace.utils.CodeUtil;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @ToString
 public class Bus {
@@ -13,6 +16,10 @@ public class Bus {
     private String plateNo;
     private String plateType;
     private String plateTypeName;
+
+    public Bus(){
+
+    }
 
     public Bus(String routeName, String routeId) {
         this.routeName = routeName;
@@ -32,5 +39,29 @@ public class Bus {
 
     public int plateNoDiff(Bus bus) {
         return this.plateNo.compareTo(bus.getPlateNo());
+    }
+
+    public List<Bus> getBusList() {
+        List<Bus> busList = new ArrayList<>();
+        Bus bus = new Bus("3200", "224000019");
+        busList.add(bus);
+        bus = new Bus("3300", "224000047");
+        busList.add(bus);
+        bus = new Bus("3400", "224000050");
+        busList.add(bus);
+        bus = new Bus("3500", "224000054");
+        busList.add(bus);
+        bus = new Bus("5602", "216000047");
+        busList.add(bus);
+        bus = new Bus("5604", "224000040");
+        busList.add(bus);
+        bus = new Bus("32", "217000009");
+        busList.add(bus);
+        bus = new Bus("81", "208000009");
+        busList.add(bus);
+        bus = new Bus("30-2", "224000014");
+        busList.add(bus);
+
+        return busList;
     }
 }

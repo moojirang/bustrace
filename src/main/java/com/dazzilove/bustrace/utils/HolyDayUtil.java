c8eaea3d9d3cf5b8cdce8d5cb0732875a488eb3f
10028467 <dazzilove@sk.com>
2020-01-31 19:21:21 +0900

    - 운행대수 정보 알람 기능 추가 중 -- 휴일정보 리턴 관련 구현 필요 함

---
+++ src/main/java/com/dazzilove/bustrace/utils/HolyDayUtil.java
@@ -0,0 +1,15 @@
+package com.dazzilove.bustrace.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HolyDayUtil {
+
+    private Map<String, String> holydays = new HashMap<>();
+
+    public static boolean isHolyDay(String yyyymmdd) {
+        // TODO 기능구현 필요함
+        return false;
+    }
+
+}

3f39625a7b717edf839d412240998920f9fdb7e3
10028467 <dazzilove@sk.com>
2020-03-09 00:46:13 +0900

    - id 추가

--- src/main/java/com/dazzilove/bustrace/domain/Station.java
+++ src/main/java/com/dazzilove/bustrace/domain/Station.java
@@ -2,13 +2,17 @@ package com.dazzilove.bustrace.domain;
 
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @ToString
 public class Station {
+	@Id private UUID id;
+	private String routeId;
 	private String stationId;
 	private String stationSeq;
 	private String stationName;

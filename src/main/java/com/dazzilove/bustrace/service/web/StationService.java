8c7ee4edc6e93c1d4f74241a05296f1c081a2d62
10028467 <dazzilove@sk.com>
2020-03-09 00:45:08 +0900

    - 최초 커밋

---
+++ src/main/java/com/dazzilove/bustrace/service/web/StationService.java
@@ -0,0 +1,7 @@
+package com.dazzilove.bustrace.service.web;
+
+import com.dazzilove.bustrace.domain.Station;
+
+public interface StationService {
+    Station getStation(String stationId);
+}
---
+++ src/main/java/com/dazzilove/bustrace/service/web/StationServiceImpl.java
@@ -0,0 +1,38 @@
+package com.dazzilove.bustrace.service.web;
+
+import com.dazzilove.bustrace.domain.Station;
+import com.dazzilove.bustrace.repository.StationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StationServiceImpl implements StationService {
+
+    @Autowired
+    MongoOperations mongoOperations;
+
+    @Autowired
+    StationRepository stationRepository;
+
+    @Override
+    public Station getStation(String stationId) {
+        List<AggregationOperation> list = new ArrayList<AggregationOperation>();
+        if (stationId.length() > 0)
+            list.add(Aggregation.match(Criteria.where("stationId").is(stationId)));
+        TypedAggregation<Station> agg = Aggregation.newAggregation(Station.class, list);
+
+        List<Station> stations = mongoOperations.aggregate(agg, Station.class, Station.class).getMappedResults();
+        if (stations == null || stations.size() == 0)
+            return new Station();
+        else
+            return stations.get(0);
+    }
+}

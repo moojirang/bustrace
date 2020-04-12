d2ef9c11c72fb24f19dcf786da14ff89c52d83eb
dazzilove <dazzilove@gmail.com>
2020-04-12 21:38:56 +0900

    application.pid 설정 추가

--- src/main/java/com/dazzilove/bustrace/service/wsdl/package-info.java
+++ src/main/java/com/dazzilove/bustrace/service/wsdl/package-info.java
@@ -2,7 +2,7 @@
 // 이 파일은 JAXB(JavaTM Architecture for XML Binding) 참조 구현 2.3.0 버전을 통해 생성되었습니다. 
 // <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a>를 참조하십시오. 
 // 이 파일을 수정하면 소스 스키마를 재컴파일할 때 수정 사항이 손실됩니다. 
-// 생성 날짜: 2019.12.16 시간 11:52:07 PM KST 
+// 생성 날짜: 2020.04.12 시간 08:41:51 PM KST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.api.javaweb/")
